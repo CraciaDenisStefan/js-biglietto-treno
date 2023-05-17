df07d64bf20932c52e2ef771f8be5f68cc73dc61
CraciDenisStefan <crdenis13@gmail.com>
2023-05-17 15:21:47 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let km_da_fare = parseInt(prompt(`Quanti km deve percorre?`));
+
+let eta = parseInt(prompt(`Quanti anni ha?`));
+
+let prezzo_km = (0.21).toFixed(2) ;
+
+let prezzo = km_da_fare * prezzo_km 
+
+if(eta <= 18) {
+     sconto_20 =  prezzo * 20 / 100 ;
+     prezzo = (prezzo - sconto_20).toFixed(2);
+}else if(eta >= 65) {
+    sconto_40 =  prezzo * 40 / 100 ;
+    prezzo = (prezzo - sconto_40).toFixed(2);
+}
+   
+document.getElementById(`info`).innerHTML = prezzo;
