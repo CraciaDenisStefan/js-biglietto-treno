07431d4735218e8c19d2e14a567cc7c196d9d963
CraciDenisStefan <crdenis13@gmail.com>
2023-05-18 15:26:42 +0200

    [FIX]solo prezzo stampato in pagina - toFixed 2

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ let eta = parseInt(prompt(`Quanti anni ha?`));
 
 let prezzo_km = (0.21);
 
-let prezzo = (km_da_fare * prezzo_km).toFixed(2); 
+let prezzo = (km_da_fare * prezzo_km); 
 
 if(eta <= 18) {
      sconto_20 =  prezzo * 20 / 100 ;
-     prezzo = (prezzo - sconto_20).toFixed(2);
+     prezzo = (prezzo - sconto_20);
 }else if(eta >= 65) {
     sconto_40 =  prezzo * 40 / 100 ;
-    prezzo = (prezzo - sconto_40).toFixed(2);
+    prezzo = (prezzo - sconto_40);
 }
    
-document.getElementById(`info`).innerHTML = prezzo;
+document.getElementById(`info`).innerHTML = (prezzo).toFixed(2);
