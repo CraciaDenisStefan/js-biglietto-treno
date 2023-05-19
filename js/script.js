b350350809316c842c7ba60c1333773738c52e1c
CraciDenisStefan <crdenis13@gmail.com>
2023-05-19 10:29:19 +0200

    Variabili dichiarate ( avevo dimenticato il push )

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ let prezzo_km = (0.21);
 let prezzo = (km_da_fare * prezzo_km); 
 
 if(eta <= 18) {
-     sconto_20 =  prezzo * 20 / 100 ;
+    let sconto_20 =  prezzo * 20 / 100 ;
      prezzo = (prezzo - sconto_20);
 }else if(eta >= 65) {
-    sconto_40 =  prezzo * 40 / 100 ;
-    prezzo = (prezzo - sconto_40);
+    let sconto_40 =  prezzo * 40 / 100 ;
+     prezzo = (prezzo - sconto_40);
 }
    
 document.getElementById(`info`).innerHTML = (prezzo).toFixed(2);
